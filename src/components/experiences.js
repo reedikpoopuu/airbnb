0a99b7b64ec226fe1f169c18bf25cc5073d18385
Reedik Poopuu <reedik.poopuu@gmail.com>
2022-08-29 19:51:41 +0300

    Add Poppinds font

---
+++ src/components/experiences.js
@@ -0,0 +1,8 @@
+export default function Experiences() {
+    return(
+        <div className="experiences">
+        <h1 className="experiences--title">Online Experiences</h1>
+        <p className="experiences--text">Join unique interactive activites led by one-of-a-kind hosts - all without leaving home.</p>
+        </div>
+    )
+}
---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import logo from "../images/airbnb-logo.png"
+
+export default function Header() {
+    return(
+        <nav>
+            <img src={logo} alt="logo"></img>
+        </nav>
+    )
+}
